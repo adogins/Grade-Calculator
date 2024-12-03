064ce93fd5036653b4da90ccd957e9efdf65779b
Courtney Adams <courtneykadams7@gmail.com>
2024-12-03 10:46:04 -0500

    updated schema

--- grade-calculator/src/models/UserSchema.ts
+++ grade-calculator/src/models/UserSchema.ts
@@ -1,7 +1,7 @@
 import mongoose, { Schema, Document, Model } from "mongoose";
 
-interface User extends Document {
-  userId: string;
+interface IUser extends Document {
+  name: string;
   username: string;
   email: string;
   password: string;
@@ -9,24 +9,22 @@ interface User extends Document {
 }
 
 interface Course {
-  courseId: string;
   courseName: string;
   courseNumber: string;
   professor: string;
   syllabus: string;
   image: string;
+  finalGrade: number; 
   categories: Category[]; // Array of categories
 }
 
 interface Category {
-  categoryId: string;
   categoryName: string;
   weight: number; // Percentage weight of this category
   assignments: Assignment[]; // Array of assignments
 }
 
 interface Assignment {
-  assignmentId: string;
   assignmentName: string;
   grade: number;
 }
@@ -34,36 +32,34 @@ interface Assignment {
 // Mongoose Schemas
 
 const AssignmentSchema = new Schema<Assignment>({
-  assignmentId: { type: String, required: true },
   assignmentName: { type: String, required: true },
   grade: { type: Number, required: true },
 });
 
 const CategorySchema = new Schema<Category>({
-  categoryId: { type: String, required: true },
   categoryName: { type: String, required: true },
   weight: { type: Number, required: true },
   assignments: { type: [AssignmentSchema], default: [] },
 });
 
 const CourseSchema = new Schema<Course>({
-  courseId: { type: String, required: true },
   courseName: { type: String, required: true },
   courseNumber: { type: String, required: true },
   professor: { type: String, required: true },
-  syllabus: { type: String, requires: true },
-  image: { type: String, requires: true },
+  syllabus: { type: String, required: true },
+  image: { type: String, required: true },
+  finalGrade: { type: Number, required: true},
   categories: { type: [CategorySchema], default: [] },
 });
 
-const UserSchema = new Schema<User>({
-  userId: { type: String, required: true },
+const UserSchema = new Schema<IUser>({
+  name: {type: String, required: true},
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   courses: { type: [CourseSchema], default: [] },
 });
 
-const User: Model<User> = mongoose.models.User || mongoose.model<User>("User", UserSchema);
+const User: Model<IUser> = mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
 export default User;
 
 
