160efc104355804872ee62d28a0ccb33c84b243e
adogins <124842665+adogins@users.noreply.github.com>
2024-12-03 23:16:24 -0500

    Reloaded website

--- grade-calculator/src/middleware.ts
+++ grade-calculator/src/middleware.ts
@@ -6,6 +6,17 @@ const { auth } = NextAuth(authConfig);
 
 export async function middleware(request: any) {
     const { nextUrl } = request;
+    const pathname = nextUrl.pathname;
+
+    if (pathname.startsWith('/_next/') || pathname.startsWith('/static/') || pathname === '/favicon.ico') {
+        return NextResponse.next();
+      }
+    
+      // Skip authentication check for public routes (like login page)
+      if (pathname === "/" || pathname === "/Signup" || pathname === "/api/users") {
+        return NextResponse.next();
+      }
+
     const session = await auth();
     const isAuthenticated = !!session?.user;
     console.log(isAuthenticated, nextUrl.pathname);
@@ -20,6 +31,8 @@ export async function middleware(request: any) {
 /*export const config = {
     matcher: [
         "/api/users/",
-        "/api/users/[userId]/:path*"
+        "/api/users/[userId]/:path*",
+        "/Signup",
+        "/CourseView"
     ],
 };*/
